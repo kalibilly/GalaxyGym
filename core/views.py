42a8516d63a9fbf19110522c4576122d1163fead
kalibilly <arund1483@gmail.com>
2026-05-15 22:26:49 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,13 +1,66 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
+from accounts.permissions import RoleRequiredMixin
+from accounts.models import UserAccount
 from .services import get_dashboard_metrics
 
 
 class DashboardHomeView(LoginRequiredMixin, TemplateView):
+    """
+    Main dashboard view that routes to role-specific dashboards.
+    """
     template_name = 'dashboard/home.html'
+
+    def get(self, request, *args, **kwargs):
+        """Route to role-specific dashboard."""
+        role = getattr(request.user, 'role', None)
+        
+        if role == UserAccount.ROLE_OWNER:
+            return redirect('dashboard_owner')
+        elif role == UserAccount.ROLE_STAFF:
+            return redirect('dashboard_staff')
+        elif role == UserAccount.ROLE_MEMBER:
+            return redirect('dashboard_member')
+        
+        return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(get_dashboard_metrics())
         return context
+
+
+class MemberDashboardView(RoleRequiredMixin, TemplateView):
+    """Dashboard for members."""
+    template_name = 'dashboard/member_dashboard.html'
+    allowed_roles = [UserAccount.ROLE_MEMBER]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['role_display'] = self.request.user.get_role_display()
+        return context
+
+
+class StaffDashboardView(RoleRequiredMixin, TemplateView):
+    """Dashboard for staff."""
+    template_name = 'dashboard/staff_dashboard.html'
+    allowed_roles = [UserAccount.ROLE_STAFF, UserAccount.ROLE_OWNER]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['role_display'] = self.request.user.get_role_display()
+        return context
+
+
+class OwnerDashboardView(RoleRequiredMixin, TemplateView):
+    """Dashboard for owners."""
+    template_name = 'dashboard/owner_dashboard.html'
+    allowed_roles = [UserAccount.ROLE_OWNER]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['role_display'] = self.request.user.get_role_display()
+        return context
+
