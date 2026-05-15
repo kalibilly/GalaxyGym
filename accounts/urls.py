8c10585cc6e797e5701b2931897e5ad761567c5b
kalibilly <arund1483@gmail.com>
2026-05-15 19:38:23 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('signup/', views.CustomSignupView.as_view(), name='signup'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('profile/', views.profile_view, name='profile'),
 ]
