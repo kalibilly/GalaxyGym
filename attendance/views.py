bb935e53ed07ac3cc61209bdc8a98cbb3c68a861
kalibilly <arund1483@gmail.com>
2026-05-23 22:05:34 +0100

    Update views.py

--- attendance/views.py
+++ attendance/views.py
@@ -1,14 +1,53 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
+from accounts.models import UserAccount
 from .forms import AttendanceLogForm
 from .models import AttendanceLog
 
 
-class AttendanceListView(LoginRequiredMixin, ListView):
+class AttendanceAccessMixin(LoginRequiredMixin):
+    def get_member_profile(self):
+        return getattr(self.request.user, 'member_profile', None)
+
+    def get_staff_profile(self):
+        return getattr(self.request.user, 'staff_profile', None)
+
+    def is_owner(self):
+        return getattr(self.request.user, 'role', None) == UserAccount.ROLE_OWNER
+
+    def is_member(self):
+        return getattr(self.request.user, 'role', None) == UserAccount.ROLE_MEMBER
+
+    def is_staff_user(self):
+        return getattr(self.request.user, 'role', None) == UserAccount.ROLE_STAFF
+
+    def get_allowed_attendance_queryset(self, queryset):
+        if self.is_owner():
+            return queryset
+        if self.is_member():
+            member = self.get_member_profile()
+            return queryset.filter(member=member) if member else queryset.none()
+        if self.is_staff_user():
+            staff = self.get_staff_profile()
+            return queryset.filter(staff=staff) if staff else queryset.none()
+        return queryset.none()
+
+    def ensure_attendance_access(self, attendance):
+        if self.is_owner():
+            return attendance
+        if self.is_member() and attendance.member == self.get_member_profile():
+            return attendance
+        if self.is_staff_user() and attendance.staff == self.get_staff_profile():
+            return attendance
+        raise PermissionDenied('You are not authorized to access this attendance record.')
+
+
+class AttendanceListView(AttendanceAccessMixin, ListView):
     model = AttendanceLog
     template_name = 'attendance/attendance_list.html'
     context_object_name = 'attendance_list'
@@ -34,7 +73,7 @@ class AttendanceListView(LoginRequiredMixin, ListView):
                 | Q(staff__full_name__icontains=query)
                 | Q(staff__staff_id__icontains=query)
             )
-        return queryset
+        return self.get_allowed_attendance_queryset(queryset)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -59,23 +98,32 @@ class AttendanceUpdateView(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('attendance:attendance_list')
 
 
-class AttendanceDetailView(LoginRequiredMixin, DetailView):
+class AttendanceDetailView(AttendanceAccessMixin, DetailView):
     model = AttendanceLog
     template_name = 'attendance/attendance_detail.html'
     context_object_name = 'attendance'
+
+    def get_object(self, queryset=None):
+        attendance = super().get_object(queryset=queryset)
+        return self.ensure_attendance_access(attendance)
 
 
 from members.models import Member
 from staffs.models import Staff
 
 
-class MemberAttendanceHistoryView(LoginRequiredMixin, DetailView):
+class MemberAttendanceHistoryView(AttendanceAccessMixin, DetailView):
     model = Member
     template_name = 'attendance/member_history.html'
     context_object_name = 'member'
 
     def get_object(self):
-        return get_object_or_404(Member, pk=self.kwargs['member_pk'])
+        member = get_object_or_404(Member, pk=self.kwargs['member_pk'])
+        if self.is_owner():
+            return member
+        if self.is_member() and member == self.get_member_profile():
+            return member
+        raise PermissionDenied('You do not have permission to view another member\'s attendance history.')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -84,13 +132,18 @@ class MemberAttendanceHistoryView(LoginRequiredMixin, DetailView):
         return context
 
 
-class StaffAttendanceHistoryView(LoginRequiredMixin, DetailView):
+class StaffAttendanceHistoryView(AttendanceAccessMixin, DetailView):
     model = Staff
     template_name = 'attendance/staff_history.html'
     context_object_name = 'staff'
 
     def get_object(self):
-        return get_object_or_404(Staff, pk=self.kwargs['staff_pk'])
+        staff = get_object_or_404(Staff, pk=self.kwargs['staff_pk'])
+        if self.is_owner():
+            return staff
+        if self.is_staff_user() and staff == self.get_staff_profile():
+            return staff
+        raise PermissionDenied('You do not have permission to view another staff member\'s attendance history.')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
