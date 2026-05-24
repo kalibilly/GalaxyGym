6aab24c57a3cf059bfc1aaba04cf1c6a8d67bc71
kalibilly <arund1483@gmail.com>
2026-05-24 06:22:03 +0100

    Update views.py

--- memberships/views.py
+++ memberships/views.py
@@ -1,10 +1,20 @@
+from datetime import date, timedelta
+from decimal import Decimal
+import uuid
+
+from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import PermissionDenied
 from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic import CreateView, DetailView, FormView, ListView, UpdateView
 
-from .forms import MembershipForm, MembershipPlanForm
+from payments.gateway import RazorpayGateway, RazorpayGatewayConfig
+
+from .forms import MembershipForm, MembershipPlanForm, MembershipPurchaseForm
 from .models import Membership, MembershipPlan
 
 
@@ -47,6 +57,109 @@ class MembershipPlanDetailView(LoginRequiredMixin, DetailView):
     model = MembershipPlan
     template_name = 'memberships/plan_detail.html'
     context_object_name = 'plan'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['can_purchase'] = getattr(self.request.user, 'member_profile', None) is not None and self.object.is_active
+        return context
+
+
+class MembershipPurchaseView(LoginRequiredMixin, FormView):
+    template_name = 'memberships/plan_purchase.html'
+    form_class = MembershipPurchaseForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.plan = get_object_or_404(MembershipPlan, pk=self.kwargs['pk'], is_active=True)
+        if not getattr(request.user, 'member_profile', None):
+            raise PermissionDenied('Only members can purchase membership plans online.')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['balance_due_date'] = date.today() + timedelta(days=7)
+        return initial
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['plan'] = self.plan
+        context['gateway_enabled'] = self._get_gateway() is not None
+        return context
+
+    def _get_gateway(self):
+        api_key = getattr(settings, 'RAZORPAY_API_KEY', None)
+        api_secret = getattr(settings, 'RAZORPAY_API_SECRET', None)
+        if not api_key or not api_secret:
+            return None
+        config = RazorpayGatewayConfig(api_key=api_key, api_secret=api_secret, test_mode=getattr(settings, 'RAZORPAY_TEST_MODE', True))
+        return RazorpayGateway(config)
+
+    def _generate_invoice_no(self):
+        return f'INV-{uuid.uuid4().hex[:8].upper()}'
+
+    def form_valid(self, form):
+        member = self.request.user.member_profile
+        start_date = date.today()
+        end_date = start_date + timedelta(days=self.plan.duration_days)
+
+        membership = Membership.objects.create(
+            member=member,
+            plan=self.plan,
+            start_date=start_date,
+            end_date=end_date,
+            membership_amount=self.plan.price,
+            discount_amount=0,
+            remarks=form.cleaned_data['remarks'],
+        )
+
+        invoice = self._create_invoice(member, membership, form.cleaned_data['balance_due_date'])
+        order_data = self._create_razorpay_order(invoice)
+
+        messages.success(self.request, 'Your membership purchase has been created. Review the invoice to complete payment.')
+
+        context = self.get_context_data(
+            form=form,
+            plan=self.plan,
+            purchase_created=True,
+            membership=membership,
+            invoice=invoice,
+            order_data=order_data,
+        )
+        return self.render_to_response(context)
+
+    def _create_invoice(self, member, membership, balance_due_date):
+        from payments.models import Invoice
+
+        invoice = Invoice.objects.create(
+            invoice_no=self._generate_invoice_no(),
+            member=member,
+            membership=membership,
+            invoice_date=date.today(),
+            due_date=balance_due_date,
+            subtotal=self.plan.price,
+            discount_amount=0,
+            tax_amount=0,
+            remarks=f'Online membership purchase for {self.plan.name}.',
+        )
+        return invoice
+
+    def _create_razorpay_order(self, invoice):
+        gateway = self._get_gateway()
+        if not gateway or invoice.balance_amount <= 0:
+            return None
+
+        try:
+            return gateway.create_order(
+                amount_paise=int(Decimal(invoice.balance_amount) * Decimal('100')),
+                receipt=invoice.invoice_no,
+                notes={
+                    'member_id': invoice.member.member_id,
+                    'membership_id': str(invoice.membership_id),
+                    'invoice_id': str(invoice.pk),
+                },
+            )
+        except Exception:
+            messages.warning(self.request, 'Could not initialize Razorpay checkout at this time.')
+            return None
 
 
 class MembershipListView(LoginRequiredMixin, ListView):
@@ -96,3 +209,8 @@ class MembershipDetailView(LoginRequiredMixin, DetailView):
     model = Membership
     template_name = 'memberships/membership_detail.html'
     context_object_name = 'membership'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_invoice'] = self.object.invoices.first()
+        return context
