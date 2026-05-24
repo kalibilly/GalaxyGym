142086b74f52a85bbae029ea45485b17e70296e4
kalibilly <arund1483@gmail.com>
2026-05-24 21:36:05 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from .views import (
     DashboardHomeView,
-    MemberDashboardView,
     StaffDashboardView,
     OwnerDashboardView,
 )
 
 urlpatterns = [
     path('', DashboardHomeView.as_view(), name='dashboard'),
-    path('member/', MemberDashboardView.as_view(), name='dashboard_member'),
     path('staff/', StaffDashboardView.as_view(), name='dashboard_staff'),
     path('owner/', OwnerDashboardView.as_view(), name='dashboard_owner'),
 ]
