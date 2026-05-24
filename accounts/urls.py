eb512e64444f846b7eba293d4a23d4987ca622fe
kalibilly <arund1483@gmail.com>
2026-05-24 21:33:58 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,6 @@ from . import views
 from .views import DashboardHomeView
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('signup/', views.SignupRequestView.as_view(), name='signup'),
-    path('signup/success/', views.signup_success_view, name='signup_success'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('dashboard/', DashboardHomeView.as_view(), name='dashboard'),
     path('profile/', views.profile_view, name='profile'),
