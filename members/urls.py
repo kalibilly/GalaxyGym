489bd6ed37e9e3f729074b6e4966ac8c3ec17753
kalibilly <arund1483@gmail.com>
2026-05-25 00:11:20 +0100

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -7,6 +7,9 @@ app_name = 'members'
 urlpatterns = [
     path('', views.MemberListView.as_view(), name='list'),
     path('create/', views.MemberCreateView.as_view(), name='create'),
+    path('<int:pk>/request-delete/', views.MemberDeleteRequestCreateView.as_view(), name='request_delete'),
+    path('delete-requests/', views.MemberDeleteRequestListView.as_view(), name='delete_request_list'),
+    path('delete-requests/<int:pk>/', views.MemberDeleteRequestReviewView.as_view(), name='delete_request_review'),
     path('<int:pk>/', views.MemberDetailView.as_view(), name='detail'),
     path('<int:pk>/update/', views.MemberUpdateView.as_view(), name='update'),
 ]
