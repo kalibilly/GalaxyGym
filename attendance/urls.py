c2e94b37edf36317bea011c0c109891393b23990
kalibilly <arund1483@gmail.com>
2026-05-24 06:19:17 +0100

    Update urls.py

--- attendance/urls.py
+++ attendance/urls.py
@@ -6,9 +6,7 @@ app_name = 'attendance'
 
 urlpatterns = [
     path('', views.AttendanceListView.as_view(), name='attendance_list'),
-    path('create/', views.AttendanceCreateView.as_view(), name='attendance_create'),
     path('<int:pk>/', views.AttendanceDetailView.as_view(), name='attendance_detail'),
-    path('<int:pk>/update/', views.AttendanceUpdateView.as_view(), name='attendance_update'),
     path('member/<int:member_pk>/', views.MemberAttendanceHistoryView.as_view(), name='member_history'),
     path('staff/<int:staff_pk>/', views.StaffAttendanceHistoryView.as_view(), name='staff_history'),
 ]
