b671c603594ce14a5a37407e5551ef740b18f1dc
kalibilly <arund1483@gmail.com>
2026-05-24 21:37:58 +0100

    Update urls.py

--- memberships/urls.py
+++ memberships/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path('plans/', views.MembershipPlanListView.as_view(), name='plan_list'),
     path('plans/create/', views.MembershipPlanCreateView.as_view(), name='plan_create'),
     path('plans/<int:pk>/', views.MembershipPlanDetailView.as_view(), name='plan_detail'),
-    path('plans/catalog/', views.MembershipPlanCatalogView.as_view(), name='plan_catalog'),
-    path('plans/<int:pk>/purchase/', views.MembershipPurchaseView.as_view(), name='plan_purchase'),
-    path('plans/purchase-success/<int:invoice_pk>/', views.MembershipPurchaseSuccessView.as_view(), name='purchase_success'),
     path('plans/<int:pk>/update/', views.MembershipPlanUpdateView.as_view(), name='plan_update'),
 
     path('', views.MembershipListView.as_view(), name='list'),
