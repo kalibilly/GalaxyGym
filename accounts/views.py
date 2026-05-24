d53fb8afd543c2210d1cc9ba5bd4c47f23e17388
kalibilly <arund1483@gmail.com>
2026-05-24 21:35:11 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
-from .forms import LoginForm, SignupRequestForm, UserProfileForm
+from .forms import LoginForm, UserProfileForm
 from .models import UserAccount
 
 
@@ -31,37 +31,6 @@ class CustomPasswordChangeView(PasswordChangeView):
     def form_valid(self, form):
         messages.success(self.request, 'Your password was changed successfully.')
         return super().form_valid(form)
-
-
-class SignupRequestView(FormView):
-    """
-    View for users to submit a signup request.
-    Request is saved but account is not created until admin approval.
-    """
-    template_name = 'registration/signup.html'
-    form_class = SignupRequestForm
-    success_url = reverse_lazy('signup_success')
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect('home')
-        return super().dispatch(request, *args, **kwargs)
-
-    def form_valid(self, form):
-        form.save()
-        messages.success(
-            self.request,
-            'Your signup request has been submitted successfully. '
-            'The gym will verify your details and notify you on WhatsApp within 1-2 business days.'
-        )
-        return super().form_valid(form)
-
-
-def signup_success_view(request):
-    """
-    Success page after signup request submission.
-    """
-    return render(request, 'registration/signup_success.html')
 
 
 class CustomLogoutView(LogoutView):
