6b415c23380430f418593d1a1b3f738ee19de80b
kalibilly <arund1483@gmail.com>
2026-05-28 17:22:23 +0530

    Update urls.py

--- galaxygym/urls.py
+++ galaxygym/urls.py
@@ -3,10 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import HomeView, EventListView
+from attendance.views import biometric_endpoint
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/biometric/', biometric_endpoint, name='api_biometric'),
+    path('iclock/cdata', biometric_endpoint, name='iclock_cdata'),
+    path('cdata', biometric_endpoint, name='cdata'),
+    path('iWsService', biometric_endpoint, name='iws_service'),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('core.urls')),
     path('departments/', include('departments.urls')),
@@ -15,9 +19,8 @@ urlpatterns = [
     path('memberships/', include('memberships.urls')),
     path('payments/', include('payments.urls')),
     path('attendance/', include('attendance.urls')),
-    path('events/', EventListView.as_view(), name='events'),
     path('notifications/', include('notifications.urls')),
-    path('', HomeView.as_view(), name='home'),
+    path('', lambda request: __import__('django.shortcuts', fromlist=['redirect']).redirect('dashboard')),
 ]
 
 if settings.DEBUG:
