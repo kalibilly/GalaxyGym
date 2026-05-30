a13432c32415497615a0fa026e3b53177a37b63e
kalibilly <arund1483@gmail.com>
2026-05-30 14:09:45 +0100

    Update api_urls.py

--- attendance/api_urls.py
+++ attendance/api_urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import access_sync, biometric_endpoint, bridge_entry
+from .views import access_sync, biometric_endpoint, biometric_get_request, bridge_entry
 
 app_name = 'attendance_api'
 
 urlpatterns = [
     path('', biometric_endpoint, name='biometric_root'),
+    path('getrequest', biometric_get_request, name='getrequest'),
+    path('iclock/getrequest', biometric_get_request, name='iclock_getrequest'),
     path('iclock/cdata', biometric_endpoint, name='iclock_cdata'),
     path('cdata', biometric_endpoint, name='cdata'),
     path('iWsService', biometric_endpoint, name='iwsservice'),
