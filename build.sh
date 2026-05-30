90b0d9187efe7661b1721327cacc83adc7b63f20
kalibilly <arund1483@gmail.com>
2026-05-30 09:58:14 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
-python manage.py makemigrations --noinput
+# python manage.py makemigrations --noinput
 
 python manage.py migrate --noinput
 
