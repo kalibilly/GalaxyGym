88512a72e2d635cf84c69e0f9c9c7d834c952d62
kalibilly <arund1483@gmail.com>
2026-06-20 08:00:27 -0700

    Update api_urls.py

--- attendance/api_urls.py
+++ attendance/api_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
     access_sync,
+    biometric_device_cdata,
     biometric_endpoint,
     biometric_get_request,
     bridge_entry,
@@ -15,24 +16,24 @@ urlpatterns = [
     path("getrequest", biometric_get_request, name="getrequest"),
     path("getrequest/", biometric_get_request, name="getrequest_slash"),
 
-    path("cdata", biometric_endpoint, name="cdata"),
-    path("cdata/", biometric_endpoint, name="cdata_slash"),
+    path("cdata", biometric_device_cdata, name="cdata"),
+    path("cdata/", biometric_device_cdata, name="cdata_slash"),
 
-    path("cdata.aspx", biometric_endpoint, name="cdata_aspx"),
+    path("cdata.aspx", biometric_device_cdata, name="cdata_aspx"),
     path("getrequest.aspx", biometric_get_request, name="getrequest_aspx"),
 
     path("iclock/getrequest", biometric_get_request, name="iclock_getrequest"),
     path("iclock/getrequest/", biometric_get_request, name="iclock_getrequest_slash"),
 
-    path("iclock/cdata", biometric_endpoint, name="iclock_cdata"),
-    path("iclock/cdata/", biometric_endpoint, name="iclock_cdata_slash"),
+    path("iclock/cdata", biometric_device_cdata, name="iclock_cdata"),
+    path("iclock/cdata/", biometric_device_cdata, name="iclock_cdata_slash"),
 
-    path("iclock/cdata.aspx", biometric_endpoint, name="iclock_cdata_aspx"),
+    path("iclock/cdata.aspx", biometric_device_cdata, name="iclock_cdata_aspx"),
     path("iclock/getrequest.aspx", biometric_get_request, name="iclock_getrequest_aspx"),
 
     path(
         "iclock/cdata/iclock/cdata.aspx",
-        biometric_endpoint,
+        biometric_device_cdata,
         name="nested_iclock_cdata_aspx",
     ),
     path(
