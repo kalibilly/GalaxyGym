626a702f7bb161f2bf7434eafd17a0242165c7a6
kalibilly <arund1483@gmail.com>
2026-07-02 03:12:54 -0700

    Update apps.py

--- memberships/apps.py
+++ memberships/apps.py
@@ -5,3 +5,9 @@ class MembershipsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'memberships'
     verbose_name = 'Memberships'
+
+    def ready(self):
+        try:
+            from . import signals  # noqa: F401
+        except Exception:
+            pass
