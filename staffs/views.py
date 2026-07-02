ad51ec422810626c091853a0d10e6dac4ca41594
kalibilly <arund1483@gmail.com>
2026-07-02 03:24:01 -0700

    Update views.py

--- staffs/views.py
+++ staffs/views.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.db.models import Q
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from attendance.models import AttendanceLog
 from departments.models import Department
 from .forms import StaffForm
 from .models import Staff
+from attendance.biometric import BiometricSyncService
+from attendance.models import BiometricDevice
 
 
 class StaffListView(LoginRequiredMixin, ListView):
@@ -53,6 +57,36 @@ class StaffCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     success_url = reverse_lazy('staffs:list')
     success_message = 'Staff member added successfully.'
 
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        devices = BiometricDevice.objects.filter(device_type=BiometricDevice.DeviceType.AIFACE, is_active=True)
+        for device in devices:
+            service = BiometricSyncService(device)
+            try:
+                service.update_employee_ex(self.object)
+            except Exception:
+                pass
+        messages.success(self.request, self.success_message)
+        return redirect(self.success_url)
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        devices = __import__('attendance.models', fromlist=['BiometricDevice']).BiometricDevice.objects.filter(
+            device_type=__import__('attendance.models', fromlist=['BiometricDevice']).BiometricDevice.DeviceType.AIFACE,
+            is_active=True,
+        )
+        from attendance.biometric import BiometricSyncService
+        for device in devices:
+            service = BiometricSyncService(device)
+            try:
+                service.update_employee_ex(self.object)
+            except Exception:
+                pass
+        form.save_m2m()
+        return super().form_valid(form)
+
 
 class StaffUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Staff
@@ -60,6 +94,36 @@ class StaffUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     template_name = 'staffs/staff_form.html'
     success_url = reverse_lazy('staffs:list')
     success_message = 'Staff member updated successfully.'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        devices = BiometricDevice.objects.filter(device_type=BiometricDevice.DeviceType.AIFACE, is_active=True)
+        for device in devices:
+            service = BiometricSyncService(device)
+            try:
+                service.update_employee_ex(self.object)
+            except Exception:
+                pass
+        messages.success(self.request, self.success_message)
+        return redirect(self.success_url)
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        devices = __import__('attendance.models', fromlist=['BiometricDevice']).BiometricDevice.objects.filter(
+            device_type=__import__('attendance.models', fromlist=['BiometricDevice']).BiometricDevice.DeviceType.AIFACE,
+            is_active=True,
+        )
+        from attendance.biometric import BiometricSyncService
+        for device in devices:
+            service = BiometricSyncService(device)
+            try:
+                service.update_employee_ex(self.object)
+            except Exception:
+                pass
+        form.save_m2m()
+        return super().form_valid(form)
 
 
 class StaffDetailView(LoginRequiredMixin, DetailView):
