3cad4f1d379eb1220b114260995d5a4b7fbfc1e5
kalibilly <arund1483@gmail.com>
2026-07-03 07:21:01 -0700

    Update forms.py

--- members/forms.py
+++ members/forms.py
@@ -18,6 +18,8 @@ class MemberForm(forms.ModelForm):
             'emergency_contact_phone',
             'assigned_staff',
             'address',
+            'vehicle_number_permanent',
+            'vehicle_number_temporary',
             'date_of_joining',
             'status',
             'is_active',
@@ -27,7 +29,21 @@ class MemberForm(forms.ModelForm):
             'date_of_joining': forms.DateInput(attrs={'type': 'date'}),
             'address': forms.Textarea(attrs={'rows': 3}),
             'notes': forms.Textarea(attrs={'rows': 3}),
+            'vehicle_number_permanent': forms.TextInput(attrs={'placeholder': 'Permanent vehicle number'}),
+            'vehicle_number_temporary': forms.TextInput(attrs={'placeholder': 'Temporary vehicle number'}),
+            'device_user_id': forms.HiddenInput(),
+            'member_id': forms.HiddenInput(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['device_user_id'].required = False
+        self.fields['member_id'].required = False
+        self.fields['full_name'].required = True
+        self.fields['phone_number'].required = True
+        self.fields['date_of_joining'].required = True
+        self.fields['vehicle_number_permanent'].required = False
+        self.fields['vehicle_number_temporary'].required = False
 
 
 class MemberDeleteRequestForm(forms.ModelForm):
